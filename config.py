58f99fe55869d17812eaa7885195ae353815f948
Gojo <101966680+gojosatorufanboy@users.noreply.github.com>
2024-07-16 17:25:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(environ.get("API_ID", "9976721"))
+API_HASH = environ.get("API_HASH", "3ef17a8cdb938335bd8ba292e6d816aa")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7440708748:AAEPpECqaHDx7wf0oXTcm5wWzFCR4LNLEqA")
+ADMIN = int(environ.get("ADMIN", "7172796863"))          
+CAPTION = environ.get("CAPTION", "Gojo")
 DOWNLOAD_LOCATION = "./DOWNLOADS"
